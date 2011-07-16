d6ca27615d5434bb663a9376f3571ab31e206c5d
Tobias Sommer <togo@Tobias-Sommers-MacBook.local>
2011-07-16 07:58:17 +0200

    Initial command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsgweb::Application.config.secret_token = 'bf1357b094c4f1208268a6d8627e4809b05a3e0db239b0b1bee79f6aeebc1ed3a082a77adee6a58238e0012ce85e38af0a5c87695c0b7f3149990e764d222606'
