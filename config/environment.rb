360e9057439e18d0328f871bc776912c84ebc8d2
Tobias Sommer <togo210@googlemail.com>
2011-07-17 10:56:16 +0200

    add mail support

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address  => "smtp.tsgtrippstadt.info",
-  :port  => 25,
-  :user_name  => "info@tsgtrippstadt.info",
-  :password  => "gvVRZXLBLACyD5BJ",
-  :authentication  => :login
-}
-config.action_mailer.raise_delivery_errors = true
+
 
 # Initialize the rails application
 Tsgweb::Application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,15 @@ Tsgweb::Application.configure do
   ENV['S3_SECRET']='eNKUbmGRzcJk8pn+yEXqdCKllYHVkqt4gxVmkZAt'
   ENV['S3_BUCKET']='tsgweb_production'
 
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address  => "smtp.tsgtrippstadt.info",
+    :port  => 25,
+    :user_name  => "info@tsgtrippstadt.info",
+    :password  => "gvVRZXLBLACyD5BJ",
+    :authentication  => :login
+  }
+  config.action_mailer.raise_delivery_errors = true
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
