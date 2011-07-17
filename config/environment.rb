00061ba61fe15fe93d7819768d62ef312c2ecb63
Tobias Sommer <togo210@googlemail.com>
2011-07-17 10:16:49 +0200

    add mail support

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Tsgweb::Application.initialize!
+
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['m02081c5'],
+  :password       => ENV['gvVRZXLBLACyD5BJ'],
+  :domain         => ENV['tsgtrippstadt.info']
+}
